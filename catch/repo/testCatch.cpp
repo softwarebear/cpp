01d15477ddf2ac956250113a04e7c276ee69846f
Richard Chandler <github@teddymac>
2019-02-17 21:32:34 +0000

    homogenise brackets

--- catch/repo/testCatch.cpp
+++ catch/repo/testCatch.cpp
@@ -1,11 +1,13 @@
 #define CATCH_CONFIG_MAIN
 #include "catch2/catch.hpp"
 
-unsigned int Factorial( unsigned int number ) {
+unsigned int Factorial( unsigned int number ) 
+{
     return number <= 1 ? 1 : Factorial(number-1)*number;
 }
 
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
+TEST_CASE( "Factorials are computed", "[factorial]" ) 
+{
     REQUIRE( Factorial(0) == 1 );
     REQUIRE( Factorial(1) == 1 );
     REQUIRE( Factorial(2) == 2 );
@@ -35,7 +37,8 @@ unsigned int Fibonnacci( unsigned int number )
     return thisPlace;
 }
 
-TEST_CASE( "fibonnacci sequence is computed", "[fibonnacci]" ) {
+TEST_CASE( "fibonnacci sequence is computed", "[fibonnacci]" ) 
+{
     REQUIRE( Fibonnacci(0) == 0 );
     REQUIRE( Fibonnacci(1) == 1 );
     REQUIRE( Fibonnacci(2) == 1 );
